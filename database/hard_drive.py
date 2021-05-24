3464f53504ec86f3cb7e388afbeb556dcc6310e7
max5695 <helmutmax0@gmail.com>
2021-05-24 18:07:55 +0200

    HardDrive Load and Save was added

--- database/hard_drive.py
+++ database/hard_drive.py
@@ -1,15 +1,21 @@
-
+import json
 
 
 class SaverLoader:
 
     def save(self, data, path):
         """Speichert die dict als json in den angegeben Pfad"""
-        pass
+        with open(path, 'w') as f:
+            json.dump(data, f)
+
+
+
 
     def load(self, path):
         """Ladet die json aus dem Pfad und gibt es als dict zurück"""
-        return
+        with open(path, 'r') as f:
+            dict = json.load(f)
+            return dict
 
 
 HardDrive = SaverLoader()
--- main.py
+++ main.py
@@ -3,32 +3,14 @@ from database.hard_drive import HardDrive
 
 
 # Die vorlage die bei der Klasse Member ist soll in die Datei >members/0.json< gespeichet werden (ALSO JSON)
-test_member = {
-    'first-name': 'Max',
-    'last-name': 'Helmut',
-    'member-birthday': '16.09.2005',
-    'parent-1-first-name': 'Eduard',
-    'parent-1-last-name': 'Helmut',
-    'parent-2-first-name': 'Marina',
-    'parent-2-last-name': 'Helmut',
-    'abo-category': 0,
-    'abo-cycle': 6,
-    'entry-date': '01.01.2000',
-    'cancellation-date': '',
-    'exit-date': '',
-    'street': 'Teststr',
-    'housenumber': '10',
-    'city': 'Schwelm',
-    'zip-code': '12345',
-    'contact-number': '0160/12345678',
-    'email': 'test.test@test.test',
-    'health-problems': ''
-}
-HardDrive.save(test_member, './members/0.json')
 
 
-#dict = HardDrive.load('./members/0.json')
-#member = Member(json)
+
+
+
+
+dict = HardDrive.load('./members/0.json')
+member = Member(dict)
 
 
 
